7973ca5878a6d569ce194909369218c3aef336f7
luna <lunagibson@duck.com>
2022-07-07 20:15:32 +0100

    Initial commit

---
+++ src/main/java/me/lunaluna/skirts/Skirts.java
@@ -0,0 +1,10 @@
+package me.lunaluna.skirts;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Skirts implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
